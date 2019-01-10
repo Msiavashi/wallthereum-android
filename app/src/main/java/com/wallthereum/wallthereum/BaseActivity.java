0135479083747251f2ccb9287fc24d536339a56b
ms95 <mohammad.siav@hotmail.com>
2019-01-10 19:58:19 +0330

    drawer toggler added

--- app/src/main/java/com/wallthereum/wallthereum/BaseActivity.java
+++ app/src/main/java/com/wallthereum/wallthereum/BaseActivity.java
@@ -3,12 +3,16 @@ package com.wallthereum.wallthereum;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.Toolbar;
 
 public class BaseActivity extends AppCompatActivity {
-
+    private static final String TAG = "BaseActivity";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,19 @@ public class BaseActivity extends AppCompatActivity {
         FrameLayout activityContainer = (FrameLayout) fullView.findViewById(R.id.activity_content);
         getLayoutInflater().inflate(layoutResID, activityContainer, true);
         super.setContentView(fullView);
+        this.initToolbar();
     }
+
+    private void initToolbar(){
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+        DrawerLayout drawerLayout = findViewById(R.id.activity_container);
+        setSupportActionBar(mToolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, mToolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
 
 }

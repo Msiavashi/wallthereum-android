0d12655a9a491570968ff78789776c2a9ae542a0
ms95 <mohammad.siav@hotmail.com>
2019-02-13 21:41:45 +0330

    working on loading overlay ...

--- app/src/main/java/com/wallthereum/wallthereum/MainActivity.java
+++ app/src/main/java/com/wallthereum/wallthereum/MainActivity.java
@@ -2,10 +2,13 @@ package com.wallthereum.wallthereum;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.OnClickListener;
 import com.orhanobut.dialogplus.OnItemClickListener;
 import com.orhanobut.dialogplus.ViewHolder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 
@@ -15,7 +18,7 @@ import androidx.core.view.GravityCompat;
 
 public class MainActivity extends BaseActivity {
 
-
+    private final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +35,30 @@ public class MainActivity extends BaseActivity {
         DialogPlus dialog = DialogPlus.newDialog(this)
                 .setGravity(Gravity.CENTER)
                 .setContentHolder(new ViewHolder(R.layout.new_wallet_alert))
+                .setCancelable(true)
+                .setInAnimation(R.anim.fade_in_center)
+                .setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(DialogPlus dialog, View view) {
+                        switch (view.getId()){
+                            case R.id.reject_button:
+                                dialog.dismiss();
+                                break;
+                            case R.id.accept_button:
+                                View progressOverlay = findViewById(R.id.progress_overlay);
+                                progressOverlay.setVisibility(View.VISIBLE);
+
+
+//                                dialog.dismiss();
+//                                Intent intent = new Intent(MainActivity.this, Wallet.class);
+//                                startActivity(intent);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                })
                 .create();
-            dialog.show();
+        dialog.show();
     }
 }
---
+++ app/src/main/java/com/wallthereum/wallthereum/Wallet.java
@@ -0,0 +1,15 @@
+package com.wallthereum.wallthereum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Wallet extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallet);
+    }
+
+}

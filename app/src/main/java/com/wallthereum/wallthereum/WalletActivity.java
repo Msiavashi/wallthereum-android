0489afadd029f232fbdab0495b6672a0b2716ea6
ms95 <mohammad.siav@hotmail.com>
2019-02-18 03:48:14 +0330

    Fab design and click listenet done

--- app/src/main/java/com/wallthereum/wallthereum/WalletActivity.java
+++ app/src/main/java/com/wallthereum/wallthereum/WalletActivity.java
@@ -1,15 +1,59 @@
 package com.wallthereum.wallthereum;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class WalletActivity extends BaseActivity {
+import com.joaquimley.faboptions.FabOptions;
+
+public class WalletActivity extends BaseActivity implements View.OnClickListener{
+
+    private static final String TAG = "Wallet Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
+        this.initTransactionsHistory();
+        this.initFab();
+    }
+
+    private void initFab() {
+        FabOptions fabOptions = findViewById(R.id.fab_options);
+        fabOptions.setButtonsMenu(R.menu.fab_menu);
+        fabOptions.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view){
+        /*
+            Handling wallet panel fab oprions items onClick events
+         */
+        switch (view.getId()){
+            case R.id.send_button:
+                break;
+            case R.id.receive_button:
+                break;
+            case R.id.keystore_button:
+                break;
+            case R.id.private_key_button:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void initTransactionsHistory(){
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.transactions_history);
+        TextView textView = (TextView) findViewById(R.id.empty_transactions);
+        recyclerView.setVisibility(View.GONE);
+        textView.setVisibility(View.VISIBLE);
     }
 
 }
